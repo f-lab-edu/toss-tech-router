48a6ec75dfc5833e764cd15db3d57108ed86f30a
bytrustu <bytrustu@gmail.com>
2023-03-17 18:58:27 +0900

    refactor: getArticleDetailById 결과가 없을 때 명시적으로 null 반환하도록 개선

--- src/lib/utils/articleUtils.js
+++ src/lib/utils/articleUtils.js
@@ -2,4 +2,4 @@ import { ARTICLE_DETAILS, ARTICLES } from './constant';
 
 export const getArticleList = () => ARTICLES;
 
-export const getArticleDetailById = (id) => ARTICLE_DETAILS.find((article) => article.id === id);
+export const getArticleDetailById = (id) => ARTICLE_DETAILS.find((article) => article.id === id) ?? null;
--- src/lib/utils/articleUtils.test.js
+++ src/lib/utils/articleUtils.test.js
@@ -13,20 +13,17 @@ describe('Article 데이터에 대한 단위 테스트', () => {
   describe('getArticleDetailById 함수를 테스트합니다.', () => {
     let article;
 
-    beforeEach(() => {
-      article = getArticleDetailById(1);
-    });
-
     test('id가 1인 Article Detail 데이터를 정상적으로 가져오는지 확인합니다.', () => {
       const id = 1;
       const articleLength = ARTICLE_DETAILS.length;
+      article = getArticleDetailById(1);
       expect(article).toEqual(ARTICLE_DETAILS[articleLength - id]);
     });
 
     test('id의 Article Detail 데이터가 없을 경우 undefined가 반환되는지 확인합니다.', () => {
       const id = 0;
       article = getArticleDetailById(id);
-      expect(article).toBeUndefined();
+      expect(article).toBeNull();
     });
   });
 });
