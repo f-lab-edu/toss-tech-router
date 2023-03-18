d6f60b9feb7b59dd144c90597166ba7a322da3e5
bytrustu <bytrustu@gmail.com>
2023-03-18 23:20:58 +0900

    test: articleUtils 테스트 코드 중첩, 가독성 개선 및 오류 상황 테스트 추가

--- src/lib/utils/articleUtils.test.js
+++ src/lib/utils/articleUtils.test.js
@@ -1,29 +1,48 @@
 import { getArticleList, getArticleDetailById } from './articleUtils';
 import { ARTICLE_DETAILS, ARTICLES } from './constant';
 
-describe('Article 데이터에 대한 단위 테스트', () => {
-  describe('getArticleList 함수를 테스트합니다.', () => {
-    test('Article List를 가져옵니다.', () => {
-      const articles = getArticleList();
-      expect(articles).toHaveLength(ARTICLES.length);
-      expect(articles).toEqual(ARTICLES);
-    });
+describe('getArticleList 함수를 테스트합니다.', () => {
+  test('Article List를 가져옵니다.', () => {
+    const articles = getArticleList();
+    expect(articles).toEqual(ARTICLES);
   });
 
-  describe('getArticleDetailById 함수를 테스트합니다.', () => {
-    let article;
+  test('Article List가 비어있지 않은지 확인합니다.', () => {
+    const articles = getArticleList();
+    expect(articles.length).toBeGreaterThan(0);
+  });
+});
 
-    test('id가 1인 Article Detail 데이터를 정상적으로 가져오는지 확인합니다.', () => {
-      const id = 1;
-      const articleLength = ARTICLE_DETAILS.length;
-      article = getArticleDetailById(1);
-      expect(article).toEqual(ARTICLE_DETAILS[articleLength - id]);
-    });
+describe('getArticleDetailById 함수를 테스트합니다.', () => {
+  test('id가 1인 Article Detail 데이터를 정상적으로 가져오는지 확인합니다.', () => {
+    const id = 1;
+    const articleLength = ARTICLE_DETAILS.length;
+    const expectedArticle = ARTICLE_DETAILS[articleLength - id];
+    const article = getArticleDetailById(id);
+    expect(article).toEqual(expectedArticle);
+  });
 
-    test('id의 Article Detail 데이터가 없을 경우 undefined가 반환되는지 확인합니다.', () => {
-      const id = 0;
-      article = getArticleDetailById(id);
-      expect(article).toBeNull();
-    });
+  test('id가 존재하지 않은 데이터 일 경우 null이 반환되는지 확인합니다.', () => {
+    const id = 0;
+    const article = getArticleDetailById(id);
+    expect(article).toBe(null);
+  });
+
+  test('id가 음수인 경우 null이 반환되는지 확인합니다.', () => {
+    const id = -1;
+    const article = getArticleDetailById(id);
+    expect(article).toBe(null);
+  });
+
+  test('id가 문자열인 경우 null이 반환되는지 확인합니다.', () => {
+    const id = '문자예요.';
+    const article = getArticleDetailById(id);
+    expect(article).toBe(null);
+  });
+
+  test('id가 null인 경우 null이 반환되는지 확인합니다.', () => {
+    const id = null;
+    const article = getArticleDetailById(id);
+    expect(article).toBe(null);
   });
 });
