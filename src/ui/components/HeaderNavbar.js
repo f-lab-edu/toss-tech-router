814378a705d8ad6f060da03a4e212ed6aa503d17
bytrustu <bytrustu@gmail.com>
2023-03-18 17:34:01 +0900

    feat: HeaderNavbar 컴포넌트 추가

---
+++ src/ui/components/HeaderNavbar.js
@@ -0,0 +1,62 @@
+class HeaderNavbar extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          position: sticky;
+          top: 0;
+          height: 60px;
+          border-bottom: 1px solid var(--border-grey);
+          background-color: var(--color-white);
+          z-index: 100;
+        }
+        .header {
+          display: flex;
+          justify-content: center;
+          max-width: 1140px;
+          height: 100%;
+          margin: 0 auto;
+        }
+        .nav {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          width: 92%;
+          height: 100%;
+        }
+        .nav__logo {
+          display: flex;
+          align-items: center;
+        }
+        .nav__logo--img {
+          width: auto;
+          max-width: 110px;
+          height: auto;
+        }
+      </style>
+      <header class='header'>
+        <nav class='nav'>
+          <div class='nav__logo'>
+            <a href="/" data-link="/">
+              <img class='nav__logo--img' src='https://i.imgur.com/cdzrwrt.png' alt='토스테크로고' />
+            </a>          
+          </div>
+        </nav>
+      </header>
+    `;
+  }
+}
+
+window.customElements.define('header-navbar', HeaderNavbar);
+
+export default HeaderNavbar;
